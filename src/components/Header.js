a7788cf84838a147768bfe4955920118a4b67092
renee0410 <reneewang410@gmail.com>
2023-09-10 00:05:33 +0800

    製作搜尋欄位

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,30 @@
 import Logo from '../assets/img/Logo.svg';
+import { SearchBar } from './SearchBar';
 
 export function Header() {
   return (
-    <div className="header">
+    <header>
       <div className="container">
-        <div className="navigation">
-          {/* logo */}
-            <img src={Logo} alt="" />
-          {/* 導覽列 */}
-          <nav>
-            <a href="#">Home</a>
-            <a href="#">Category</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-        </nav>
-        </div>
+        <div className="header">
+          <div className="header-l">
+                {/* logo */}
+                  <img src={Logo} alt="" />
+                {/* 導覽列 */}
+                <nav>
+                  <a href="#">Home</a>
+                  <a href="#">Category</a>
+                  <a href="#">About</a>
+                  <a href="#">Contact</a>
+                </nav>
+            </div>
+            <div className="header-r">
+              {/* 搜尋欄 */}
+              <div className="searchArea">
+                <SearchBar></SearchBar>
+              </div>
+            </div>
+          </div>
       </div>
-    </div>
+    </header>
   )
 };
