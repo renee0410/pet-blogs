735687cb57a959c0825726823620cc541dec6981
renee0410 <reneewang410@gmail.com>
2023-09-09 22:10:56 +0800

    建立路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './style/all.scss';
+import { Routes, Route } from 'react-router-dom';
 
 import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Routes>
+        <Route path='' element={<Header></Header>}></Route>
+      </Routes>
     </div>
   );
 }
