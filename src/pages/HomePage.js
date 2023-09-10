36da94eb1facbe68ff91b88d1c2f30cd923a2293
renee0410 <reneewang410@gmail.com>
2023-09-10 15:39:58 +0800

    大標題切版

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,13 @@ export function HomePage() {
   return (
     <>
       <div className="homePage">
+        {/* hero */}
         <div className="hero">
-          <div className="container"></div>
+          <div className="container">
+            <h1>One more friend</h1>
+            <h2>Thousands more fun!</h2>
+            <p>Having a pet means you have more joy, a new friend, a happy<br/>person who will always be with you to have fun. We have 200+<br/>different pets that can meet your needs!</p>
+          </div>
         </div>
       </div>
       
