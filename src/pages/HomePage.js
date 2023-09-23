17039cd3ffa456b90109f5a7584c6e5238eed3c7
renee0410 <reneewang410@gmail.com>
2023-09-23 16:11:45 +0800

    完成banner01 rwd

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,138 +16,141 @@ import Rectangle7 from '../assets/img/background/rectangle/Rectangle 7.svg';
 import Rectangle8 from '../assets/img/background/rectangle/Rectangle 8.svg';
 import Rectangle9 from '../assets/img/background/rectangle/Rectangle 9.svg';
 import Banner01 from '../assets/img/background/Banner.svg';
+import Banner01Mobile from '../assets/img/background/Banner01Mobile.svg';
 
 export function HomePage() {
   return (
     <>
       <div className="homePage">
-        {/* hero */}
-        <div className="hero">
-          {/* 背景 */}
-          <div className="backgroundImg">
-            <img src={Human} className="imgStyle" alt="" />
-          </div>
-          {/* rectangle背景 */}
-          <div className="img1">
-            <img src={Rectangle1} className="imgStyle" alt="" />
-          </div>
-          <div className="img2">
-            <img src={Rectangle2} className="imgStyle" alt="" />
-          </div>
-          <div className="img3">
-            <img src={Rectangle3} alt="" />
-          </div>
-          <div className="img5">
-            <img src={Rectangle5} alt="" />
-          </div>
-          <div className="img6">
-            <img src={Rectangle6} alt="" />
-          </div>
-          <div className="img7">
-            <img src={Rectangle7} alt="" />
-          </div>
-          <div className="img8">
-            <img src={Rectangle8} alt="" />
-          </div>
-          <div className="img9">
-            <img src={Rectangle9} alt="" />
-          </div>
-          <div className="container">
-            <h1>One more friend</h1>
-            <h3>Thousands more fun!</h3>
-            <p>Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+<br/>different pets that can meet your needs!</p>
-            {/* btn區塊 */}
-            <div className="btnArea">
-              <Button
-                style='btnSecondary'
-                text='View intro'
-                icon={Play}
-              ></Button>
-              <Button
-                style='btnPrimary'
-                text='Explore now'
-              ></Button>
-            </div>
-          </div>
+        <div className="container">
+        
         </div>
-        {/* whats new */}
-        <div className="whatsNew">
-          <div className="container">
-            <div className="whatsNewTop"> 
-              {/* 標題 */}
-              <div className="title">
-                <p>Whats new?</p>
-                <h5>Take a look at some of our pets</h5>
-              </div>
-              {/* btn */}
+          {/* hero */}
+          <div className="hero">
+            {/* 背景 */}
+            <div className="backgroundImg">
+              <img src={Human} className="imgStyle" alt="" />
+            </div>
+            {/* rectangle背景 */}
+            <div className="img1">
+              <img src={Rectangle1} className="imgStyle" alt="" />
+            </div>
+            <div className="img2">
+              <img src={Rectangle2} className="imgStyle" alt="" />
+            </div>
+            <div className="img3">
+              <img src={Rectangle3} alt="" />
+            </div>
+            <div className="img5">
+              <img src={Rectangle5} alt="" />
+            </div>
+            <div className="img6">
+              <img src={Rectangle6} alt="" />
+            </div>
+            <div className="img7">
+              <img src={Rectangle7} alt="" />
+            </div>
+            <div className="img8">
+              <img src={Rectangle8} alt="" />
+            </div>
+            <div className="img9">
+              <img src={Rectangle9} alt="" />
+            </div>
+            <div className="container">
+              <h1>One more friend</h1>
+              <h3>Thousands more fun!</h3>
+              <p>Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+<br/>different pets that can meet your needs!</p>
+              {/* btn區塊 */}
               <div className="btnArea">
                 <Button
                   style='btnSecondary'
-                  text='View more'
-                  icon={ArrowRight}
+                  text='View intro'
+                  icon={Play}
+                ></Button>
+                <Button
+                  style='btnPrimary'
+                  text='Explore now'
                 ></Button>
               </div>
             </div>
-            {/* product cards */}
-            <div className="productCardsArea">
-              <div className="row">
-                {
-                  WhatsNewData.data.map((item) => {
-                    console.log(item)
-                    return (
-                      <>
-                        <div className="col-lg-3 col-md-4">
-                          <div className="productCard">
-                            <div className="imgBoxFit">
-                              <img src={item.imgUrl} alt="" />
-                            </div>
-                            <div className="cardBody">
-                              <p className="cardTitle">{item.title}</p>
-                              <div className="cardInfo">
-                                <span className="aboutGene">Gene: {item.gene}</span>
-                                <span>•</span>
-                                <span className="aboutAge">Age: {item.age} month</span>
-                              </div>
-                              <span className="cardPrice">{item.price.toLocaleString()} VND</span>
-                            </div>
-                          </div>
-                        </div>
-                      </>
-                    )
-                  })
-                }
-              </div>
-            </div>
           </div>
-        </div>
-        {/* Banner */}
-        <div className="bannerOne">
           <div className="container">
-            <div className="banner01">
-              {/* <img src={Banner01} alt="" /> */}
-            </div>
-            <div className="row">
-              <div className="col-lg-5">
-                <div className="textArea">
-                  <h2>One more friend</h2>
-                  <h4>&nbsp;&nbsp;&nbsp;&nbsp;Thousands more fun!</h4>
-                  <span>Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!</span>
+             {/* whats new */}
+            <div className="whatsNew">
+              <div className="whatsNewTop"> 
+                {/* 標題 */}
+                <div className="title">
+                  <p>Whats new?</p>
+                  <h5>Take a look at some of our pets</h5>
                 </div>
+                {/* btn */}
                 <div className="btnArea">
                   <Button
-                    text='View Intro'
                     style='btnSecondary'
-                    icon={Play}
+                    text='View more'
+                    icon={ArrowRight}
                   ></Button>
-                  <Button
-                    style='btnPrimary'
-                    text='Explore now'
-                  ></Button>
+                </div>
+              </div>
+              {/* product cards */}
+              <div className="productCardsArea">
+                <div className="row">
+                  {
+                    WhatsNewData.data.map((item) => {
+                      console.log(item)
+                      return (
+                        <>
+                          <div className="col-lg-3 col-md-4 col-sm-6">
+                            <div className="productCard">
+                              <div className="imgBoxFit">
+                                <img src={item.imgUrl} alt="" />
+                              </div>
+                              <div className="cardBody">
+                                <p className="cardTitle">{item.title}</p>
+                                <div className="cardInfo">
+                                  <span className="aboutGene">Gene: {item.gene}</span>
+                                  <span>•</span>
+                                  <span className="aboutAge">Age: {item.age} month</span>
+                                </div>
+                                <span className="cardPrice">{item.price.toLocaleString()} VND</span>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </div>
+            {/* Banner */}
+            <div className="bannerOne">
+              <div className="banner01">
+                <img src={Banner01} className="banner01" alt="" />
+                <img src={Banner01Mobile} className="banner01Mobile" alt="" />
+              </div>
+              <div className="row">
+                <div className="col-lg-5 col-md-12 mr">
+                  <div className="textArea">
+                    <h2>One more friend</h2>
+                    <h4>Thousands more fun!</h4>
+                    <span>Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!</span>
+                  </div>
+                  <div className="btnArea">
+                    <Button
+                      text='View Intro'
+                      style='btnSecondary'
+                      icon={Play}
+                    ></Button>
+                    <Button
+                      style='btnPrimary'
+                      text='Explore now'
+                    ></Button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
       </div>
     </>
   )
